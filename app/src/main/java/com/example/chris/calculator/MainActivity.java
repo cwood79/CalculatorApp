f70d79efd92bafaa5207f31825f3bf20e54e9669
Chris <chris15618@gmail.com>
2017-01-31 15:16:08 -0800

    Created Layout

--- app/src/main/java/com/example/chris/calculator/MainActivity.java
+++ app/src/main/java/com/example/chris/calculator/MainActivity.java
@@ -5,9 +5,20 @@ import android.os.Bundle;
 
 public class MainActivity extends AppCompatActivity {
 
+    private static final char ADD = '+';
+    private static final char SUB = '-';
+    private static final char MULT = '*';
+    private static final char DIV = '/';
+    private static final char MOD = '%';
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+    }
+
+    private void compute()
+    {
+
     }
 }
